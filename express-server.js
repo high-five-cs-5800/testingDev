20bed5d403741ae78526648bf9707bc5838b27ba
EC2 Default User <ec2-user@ip-172-31-89-237.ec2.internal>
2018-11-04 21:24:50 +0000

    push4

--- express-server.js
+++ express-server.js
@@ -35,7 +35,7 @@ app.use(function(req, res, next){
     next();
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', function(req, res, next){
 //  res.send('express node server running');
